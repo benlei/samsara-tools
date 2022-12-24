093645430dbffdc18e8e7d0503901ecd40d7a4f5
Benjamin Lei <benlei.cse@gmail.com>
2022-12-24 00:45:38 -0800

    #1 - refactoring more. these helper fns are very specific to each parser fn, so moving them into the body of the parser fn

--- samsara/banners.py
+++ samsara/banners.py
@@ -11,6 +11,16 @@ def find_or_inf(s: str, substr: str, start: Optional[int]) -> int:
 
 
 def load_banners(doc: str):
+    def get_version(start: int) -> Optional[str]:
+        version_pos = doc.find('>Version', start)
+
+        # it's the end
+        if version_pos == -1:
+            return None
+
+        end_version_pos = doc.find('<', version_pos)
+        return doc[version_pos:end_version_pos].split(' ')[1]
+
     characters = {
         "5": defaultdict(lambda: dict(versions=[])),
         "4": defaultdict(lambda: dict(versions=[])),
@@ -23,7 +33,7 @@ def load_banners(doc: str):
 
     i = 0
     while i < len(doc):
-        if (version := get_version(doc, i)) is None:
+        if (version := get_version(i)) is None:
             break
 
         i = doc.find('<table', i)
@@ -43,23 +53,26 @@ def load_banners(doc: str):
 
 
 def parse_version_table(doc, end_pos, start_pos, version, characters, weapons):
-    def is_version_end(end_row_pos: int) -> bool:
-        return end_row_pos > end_pos
+    def banner_end_pos() -> int:
+        return find_or_inf(doc, '</tr', start_pos)
+
+    def is_version_end() -> bool:
+        return banner_end_pos() > end_pos
+
+    def is_weapon_banner() -> bool:
+        return find_or_inf(doc, 'Epitome Invocation', start_pos) < banner_end_pos()
 
     while start_pos < end_pos:
-        weap_banner_pos = find_or_inf(doc, 'Epitome Invocation', start_pos)
-        banner_row_end_pos = find_or_inf(doc, '</tr', start_pos)
-
         # it's the end!
-        if is_version_end(banner_row_end_pos):
+        if is_version_end():
             break
 
         # start going through the cards (char/weap)!
-        if is_weapon_banner(weap_banner_pos, banner_row_end_pos):
+        if is_weapon_banner():
             parse_banner_row(
                 doc=doc,
                 start_pos=start_pos,
-                end_pos=banner_row_end_pos,
+                end_pos=banner_end_pos(),
                 version=version,
                 store=weapons,
             )
@@ -67,16 +80,16 @@ def parse_version_table(doc, end_pos, start_pos, version, characters, weapons):
             parse_banner_row(
                 doc=doc,
                 start_pos=start_pos,
-                end_pos=banner_row_end_pos,
+                end_pos=banner_end_pos(),
                 version=version,
                 store=characters,
             )
 
-        start_pos = banner_row_end_pos + 1
+        start_pos = banner_end_pos() + 1
 
 
 def parse_banner_row(doc, end_pos, start_pos, version, store):
-    def is_row_end(five_star_pos, four_star_pos) -> bool:
+    def is_banner_end(five_star_pos, four_star_pos) -> bool:
         return min(five_star_pos, four_star_pos) > end_pos
 
     def add_version(stars: str, name: str, img_url: str):
@@ -85,61 +98,39 @@ def parse_banner_row(doc, end_pos, start_pos, version, store):
         if "image" not in store[stars][name]:
             store[stars][name]["image"] = img_url
 
+    def get_stars(five_star_pos, four_star_pos):
+        stars = "4"
+        if five_star_pos < four_star_pos:
+            stars = "5"
+        return stars
+
+    def get_img_url():
+        img_pos_start = doc.find('https://static.wikia', start_pos)
+        img_pos_end = doc.find('"', img_pos_start)
+        img_url = doc[img_pos_start:img_pos_end]
+        return img_url
+
+    def get_name():
+        title_pos_start = doc.find('title=', start_pos) + len('title="')
+        title_pos_end = doc.find('"', title_pos_start)
+        title = html.unescape(doc[title_pos_start:title_pos_end])
+        return title
+
     while start_pos < end_pos:
         five_star_pos = find_or_inf(doc, 'card_5', start_pos)
         four_star_pos = find_or_inf(doc, 'card_4', start_pos)
 
         # there are no more char/weaps
-        if is_row_end(five_star_pos, four_star_pos):
+        if is_banner_end(five_star_pos, four_star_pos):
             break
 
         # advance the pointer
         start_pos = min(five_star_pos, four_star_pos) + 1
         add_version(
             stars=get_stars(five_star_pos, four_star_pos),
-            name=get_name(doc, start_pos),
-            img_url=get_img_url(doc, start_pos),
+            name=get_name(),
+            img_url=get_img_url(),
         )
-
-
-def get_version(doc: str, start: int) -> Optional[str]:
-    version_pos = doc.find('>Version', start)
-
-    # it's the end
-    if version_pos == -1:
-        return None
-
-    end_version_pos = doc.find('<', version_pos)
-    return doc[version_pos:end_version_pos].split(' ')[1]
-
-
-def get_stars(five_star_pos, four_star_pos):
-    stars = "4"
-    if is_five_star(five_star_pos, four_star_pos):
-        stars = "5"
-    return stars
-
-
-def get_img_url(doc, i):
-    img_pos_start = doc.find('https://static.wikia', i)
-    img_pos_end = doc.find('"', img_pos_start)
-    img_url = doc[img_pos_start:img_pos_end]
-    return img_url
-
-
-def get_name(doc, i):
-    title_pos_start = doc.find('title=', i) + len('title="')
-    title_pos_end = doc.find('"', title_pos_start)
-    title = html.unescape(doc[title_pos_start:title_pos_end])
-    return title
-
-
-def is_weapon_banner(weap_pos, end_row_pos):
-    return weap_pos < end_row_pos
-
-
-def is_five_star(five_star_pos, four_star_pos):
-    return five_star_pos < four_star_pos
 
 
 def trim_doc(doc):
