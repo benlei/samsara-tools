271dae2c940c43c66af3147d73d4a15214a6b53c
Benjamin Lei <benlei.cse@gmail.com>
2022-12-27 20:08:52 -0800

    #2 - added in artifact domain parsing

--- samsara/artifacts.py
+++ samsara/artifacts.py
@@ -1,5 +1,9 @@
 def trim_doc(doc: str) -> str:
-    pass
+    start = doc.rfind(">List of Artifact Sets<")
+    start = doc.find("<table", start)
+    end = doc.find("</table", start) + len("</table>")
+
+    return doc[start:end]
 
 
 def load_artifacts(doc: str) -> dict:
