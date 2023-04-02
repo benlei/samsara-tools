69054f7855273fa57696eb794e0e774dd067357d
Benjamin Lei <benlei.cse@gmail.com>
2023-04-02 02:16:09 -0700

    saving some test progress

--- test_cli.py
+++ test_cli.py
@@ -1,13 +1,17 @@
 import json
 
+import yaml
+
 from samsara import banners, fandom
 
 
 def main() -> None:
     # fandom.MaxContinues = 5
-
-    with open("mystuff.json", "w") as f:
-        f.write(json.dumps(fandom.get_event_wishes(), indent=2))
+    # with open("mystuff.yaml", "w") as f:
+    #     f.write(yaml.dump(fandom.get_event_wishes(), default_flow_style=False))
+    # with open("mystuff.json", "w") as f:
+    #     f.write(json.dumps(fandom.get_5_star_weapons(), indent=2))
+    pass
 
 
 if __name__ == "__main__":
