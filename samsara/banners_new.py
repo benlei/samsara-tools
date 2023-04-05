173b2416d0ba27f5a1c5d1721c2ac84c0905cf66
Benjamin Lei <benlei.cse@gmail.com>
2023-04-04 23:18:59 -0700

    alright tested downloading of images, thought through how to properly generate history

---
+++ samsara/banners_new.py
@@ -0,0 +1,39 @@
+from typing import TypedDict
+
+from samsara.fandom import QueryResponse
+
+
+class BannerDates(TypedDict):
+    start: str
+    end: str
+
+
+class BannerHistory(TypedDict):
+    name: str
+    versions: list[str]
+    dates: list[BannerDates]
+
+
+class BannerDataset(TypedDict):
+    five_star_characters: BannerHistory
+    four_star_characters: BannerHistory
+    five_star_weapons: BannerHistory
+    four_star_weapons: BannerHistory
+
+
+def transform_data(
+    event_wishes_qr: QueryResponse,
+    five_star_characters_qr: QueryResponse,
+    four_star_characters_qr: QueryResponse,
+    five_star_weapons_qr: QueryResponse,
+    four_star_weapons_qr: QueryResponse,
+) -> BannerDataset:
+    # strategy (example):
+    # 1. setup separate character and weapon timeline (list)
+    #     - sorted by its version and/or maybe its date, and with maybe
+    #       its queryresponse (or some form of it)
+    #     - non-dated ones should go to the end.
+    #     - make sure to use the version parse library to compare:
+    #       https://stackoverflow.com/questions/11887762/how-do-i-compare-version-numbers-in-python
+    # 2. go through timeline and start filling out each banner history!
+    pass
--- test_cli.py
+++ test_cli.py
@@ -1,16 +1,25 @@
-import json
+import shutil
 
+import requests
 import yaml
 
-from samsara import banners, fandom
+from samsara import fandom
+from samsara.fandom import download_weapon_image
 
 
 def main() -> None:
     # fandom.MaxContinues = 5
     # with open("mystuff.yaml", "w") as f:
-    #     f.write(yaml.dump(fandom.get_event_wishes(), default_flow_style=False))
+    #     f.write(
+    #         yaml.dump(
+    #             fandom.get_5_star_characters(),
+    #             default_flow_style=False,
+    #             sort_keys=False,
+    #         )
+    #     )
     # with open("mystuff.json", "w") as f:
     #     f.write(json.dumps(fandom.get_5_star_weapons(), indent=2))
+    # download_weapon_image("./test.png", "Freedom Sworn", 100)
     pass
 
 
